ec79dcfb172001b4dcbf8cd45cdbe7e7e3d4f19f
marlooon123 <joaovictorc2506@gmail.com>
2022-10-08 12:01:48 -0300

    commi
    comi

---
+++ Assets/Scripts/colisao.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colisao : MonoBehaviour
+{
+    public CopntactFilter filtro;
+    private BoxCollider2D caixaColisao;
+    private Collider2D[] hits = new Collider2D[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        caixaColisao = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        caixaColisao.OverlapCollider(filtro, hits);
+
+        for (int i = 0; i < hits.Lenght; i++)
+        {
+            if (hits[i] == null)
+                continue;
+
+            Debug.Log(hits[i].name);
+
+            hits[i] = null;
+
+        }
+    }
+}
