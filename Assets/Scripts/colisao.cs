16ad3dcb9cf45e2a0345b19f80f632efe1894e39
marlooon123 <joaovictorc2506@gmail.com>
2022-10-08 12:14:12 -0300

    comiltu
    comiltu

--- Assets/Scripts/colisao.cs
+++ Assets/Scripts/colisao.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class colisao : MonoBehaviour
 {
-    public CopntactFilter filtro;
+    public ContactFilter2D filtro;
     private BoxCollider2D caixaColisao;
     private Collider2D[] hits = new Collider2D[10];
     // Start is called before the first frame update
@@ -18,13 +18,12 @@ public class colisao : MonoBehaviour
     {
         caixaColisao.OverlapCollider(filtro, hits);
 
-        for (int i = 0; i < hits.Lenght; i++)
+        for (int i = 0; i < hits.Length; i++)
         {
             if (hits[i] == null)
                 continue;
 
             Debug.Log(hits[i].name);
-
             hits[i] = null;
 
         }
